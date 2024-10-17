ced8f43c910292c89428d8e3a01ed4e7e1115f95
Thomas Lecoeur <thomeuxe@gmail.com>
2024-10-17 18:36:31 +0200

    fix: typing for new breakpoint option

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,7 @@ import type { MantineThemeColorsOverride } from "@mantine/core";
 import type { Config } from "tailwindcss";
 
 declare function tailwindPresetMantine(
-	options?: Partial<{ mantineColors: MantineThemeColorsOverride }>,
+	options?: Partial<{ mantineColors: MantineThemeColorsOverride, mantineBreakpoints: Partial<MantineBreakpointsValues> }>,
 ): Config;
 
 export = tailwindPresetMantine;
