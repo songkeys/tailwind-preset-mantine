3e2c6e08fa50b8ae86eaec1f4f4834a933cfeeb0
Songkeys <i@song.work>
2024-10-18 02:04:37 +0800

    fix: type import

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,14 @@
-import type { MantineThemeColorsOverride } from "@mantine/core";
+import type {
+	MantineBreakpointsValues,
+	MantineThemeColorsOverride,
+} from "@mantine/core";
 import type { Config } from "tailwindcss";
 
 declare function tailwindPresetMantine(
-	options?: Partial<{ mantineColors: MantineThemeColorsOverride, mantineBreakpoints: Partial<MantineBreakpointsValues> }>,
+	options?: Partial<{
+		mantineColors: MantineThemeColorsOverride;
+		mantineBreakpoints: Partial<MantineBreakpointsValues>;
+	}>,
 ): Config;
 
 export = tailwindPresetMantine;
